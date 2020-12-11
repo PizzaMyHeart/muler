a93d4b590cd7d1d24b7b031aa19039d8bbd47102
ian <ian_tan@hotmail.com>
2020-12-11 22:40:27 +0000

    add synonym and products buttons

---
+++ app/static/script.js
@@ -0,0 +1,16 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+
+		/* Toggle showing and hiding panel */
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block"
+		}
+	});
+}
